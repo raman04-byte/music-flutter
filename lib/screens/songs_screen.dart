dc8f5ee43a87cc3cf4adda7cb05395a85442ff95
Raman Tank <ramantank04022002@gmail.com>
2023-01-29 20:12:55 +0530

    Added a player

--- lib/screens/songs_screen.dart
+++ lib/screens/songs_screen.dart
@@ -60,6 +60,35 @@ class _SongScreenState extends State<SongScreen> {
             fit: BoxFit.cover,
           ),
           const _BackGroundFilter(),
+          MusicPlayer(
+              seekBarDataStream: _seekBarDataStream, audioPlayer: audioPlayer)
+        ],
+      ),
+    );
+  }
+}
+
+class MusicPlayer extends StatelessWidget {
+  const MusicPlayer({
+    super.key,
+    required Stream<SeekBarData> seekBarDataStream,
+    required this.audioPlayer,
+  }) : _seekBarDataStream = seekBarDataStream;
+
+  final Stream<SeekBarData> _seekBarDataStream;
+  final AudioPlayer audioPlayer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20.0,
+        vertical: 40.0,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
           StreamBuilder<SeekBarData>(
               stream: _seekBarDataStream,
               builder: (context, snapshot) {
@@ -69,12 +98,76 @@ class _SongScreenState extends State<SongScreen> {
                   duration: positionData?.duration ?? Duration.zero,
                   onChanged: audioPlayer.seek,
                 );
-              })
+              }),
+          PlayerButton(audioPlayer: audioPlayer)
         ],
       ),
     );
   }
 }
+
+class PlayerButton extends StatelessWidget {
+  const PlayerButton({
+    super.key,
+    required this.audioPlayer,
+  });
+
+  final AudioPlayer audioPlayer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        StreamBuilder(
+            stream: audioPlayer.playerStateStream,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final playerState = snapshot.data;
+                final processingState =
+                    (playerState! as PlayerState).processingState;
+                if (processingState == ProcessingState.loading ||
+                    processingState == ProcessingState.buffering) {
+                  return Container(
+                    width: 64.0,
+                    height: 64.0,
+                    margin: const EdgeInsets.all(10.0),
+                    child: const CircularProgressIndicator(),
+                  );
+                } else if (!audioPlayer.playing) {
+                  return IconButton(
+                    onPressed: audioPlayer.play,
+                    iconSize: 75,
+                    icon: const Icon(
+                      Icons.play_circle,
+                      color: Colors.white,
+                    ),
+                  );
+                } else if (processingState != ProcessingState.completed) {
+                  return IconButton(
+                    onPressed: audioPlayer.pause,
+                    iconSize: 75.0,
+                    icon: const Icon(
+                      Icons.pause_circle,
+                      color: Colors.white,
+                    ),
+                  );
+                }else{
+                return IconButton(
+                    onPressed: ()=> audioPlayer.seek(Duration.zero,index: audioPlayer.effectiveIndices!.first),
+                    iconSize: 75.0,
+                    icon: const Icon(
+                      Icons.replay_circle_filled_outlined,
+                      color: Colors.white,
+                    ),
+
+                  );
+                }
+                }else {
+                  return const CircularProgressIndicator();
+                }
+              }
+            )]);}
+  }
 
 class _BackGroundFilter extends StatelessWidget {
   const _BackGroundFilter({
--- lib/widgets/seekbar.dart
+++ lib/widgets/seekbar.dart
@@ -28,12 +28,22 @@ class SeekBar extends StatefulWidget {
 
 class _SeekBarState extends State<SeekBar> {
   double? _dragValue;
+  String _formatDuration(Duration? duration) {
+    if (duration == null) {
+      return '--:--';
+    } else {
+      String minutes = duration.inMinutes.toString().padLeft(2, '0');
+      String seconds =
+          duration.inSeconds.remainder(60).toString().padLeft(2, '0');
+      return '$minutes:$seconds';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Text('${widget.position}'),
+        Text(_formatDuration(widget.position)),
         Expanded(
           child: SliderTheme(
             data: SliderTheme.of(context).copyWith(
@@ -76,12 +86,12 @@ class _SeekBarState extends State<SeekBar> {
                     ),
                   );
                 }
-                _dragValue=null;
+                _dragValue = null;
               },
             ),
           ),
         ),
-        Text('${widget.duration}'),
+        Text(_formatDuration(widget.duration)),
       ],
     );
   }
